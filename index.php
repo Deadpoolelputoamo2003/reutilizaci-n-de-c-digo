a6808cf976c554c94b00d714003d91ff582144f0
Deadpoolelputoamo2003 <jgalocdiaz@gmail.com>
2024-10-02 22:43:24 -0500

    Primer commit

---
+++ index.php
@@ -0,0 +1,72 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="resources\css\bootstrap.min.css">
+    <link rel="stylesheet" href="resources\css\bootstrap-icons/font/bootstrap-icons.min.css">
+     <link rel="stylesheet" href="resources\css\main.css">
+</head>
+<body>
+    <!-- emcabbezado -->
+    <div class="row">
+        <div class="col-sm-12 col-md-6">
+         <h1 class="text-container">EN LINEA</h1>
+        </div>
+        <div class="col-sm-12 col-md-6">
+        <p class="text-right pr-5">
+         SOLUCIONES MUNDIALES  
+        </p>
+        </div>
+    </div>
+    <!-- fin del emcabezado -->
+     <div class="row">
+        <div class="col-sm-12">
+        <img class="w-100" style="height: 350px;" src="./resources/img/baner01.PNG" alt="">
+        </div>
+     </div>
+
+     <div class="container mt-04">
+        <h1 class="h1">SOLUCIONES CREATIVA</h1>
+        <p>Sin importar cual sea el negocio, nuestro equipo puede hacerlo todo desde cero. </p>
+     <div class="row">
+        <div class="col-sm-12 col-md-4">
+            <div class= "card border-0">
+                <div class="card-body">
+                <img class="w-100" src="./resources/img/idea_01.PNG" alt="">
+                <i class="bi bi-pencil"></i>DISEÑO</h5>
+                <p>Creamos diseños segun tus habilidades.</p>
+                </div>
+            </div>
+        </div>
+
+        <div class="col-sm-12 col-md-4 ">
+            <div class= "card border-0">
+                <div class="card-body">
+                <img class="w-100" src="./resources/img/idea_02.PNG" alt="">
+                <i class="bi bi-flower1"></i>DESARROLLO</h5>
+                <p>Nuestros programadores te garantizan la perfeccion.</p>
+                </div>
+            </div> 
+        </div>
+
+        <div class="col-sm-12 col-md-4 ">
+            <div class= "card border-0">
+                <div class="card-body">
+                <img class="w-100" src="./resources/img/idea_03.PNG" alt="">
+                <i class="bi bi-arrows-move"></i>EDICION</h5>
+                <p>Que el producto final llege a tus clientes.</p>
+                </div>
+            </div>
+        </div>
+
+     </div>
+    </div>
+     <div class="container mt-04">
+     </div>
+     <footer class="bg-wine" >
+        <p class="p-5 text white">  Edificios IDEX P3,jr.Amazonas 120 - Chachapoyas - Amazonas </p>
+     </footer>
+</body>
+</html>
